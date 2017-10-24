520f3b0b3c514b3221c0df59ca4a3e2d13501f78
Jasper van Es <jasper@0-to-9.nl>
2017-10-24 17:42:26 +0200

    Reorganised version directories

---
+++ ee3/system/ep_better_workflow/config.php
@@ -0,0 +1,12 @@
+<?php
+
+if (! defined('BWF_VER'))
+{
+	define('BWF_NAME', 'Better Workflow');
+	define('BWF_VER',  '1.6.5');
+	define('BWF_BUILD', '070715	');
+	define('BWF_LOG_FILE', PATH_THIRD . 'ep_better_workflow/logs/log.txt');
+}
+
+$config['name']    = BWF_NAME;
+$config['version'] = BWF_VER;
